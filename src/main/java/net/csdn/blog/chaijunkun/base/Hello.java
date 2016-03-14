58d872638263cb95af920db5c58b0c7c3fed8061
chaijunkun <chaijunkun@hotmail.com>
2016-03-14 18:32:49 +0800

    test

---
+++ src/main/java/net/csdn/blog/chaijunkun/base/Hello.java
@@ -0,0 +1,19 @@
+package net.csdn.blog.chaijunkun.base;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello
+ * @author chaijunkun
+ * @since 2016年3月14日
+ */
+public class Hello {
+	
+	private static final Logger log = LoggerFactory.getLogger(Hello.class);
+	
+	public void say(){
+		log.info("Hello World!");
+	}
+	
+}
---
+++ src/test/java/net/csdn/blog/chaijunkun/base/test/HelloTest.java
@@ -0,0 +1,14 @@
+package net.csdn.blog.chaijunkun.base.test;
+
+import org.junit.Test;
+
+import net.csdn.blog.chaijunkun.base.Hello;
+
+public class HelloTest {
+	
+	@Test
+	public void sayHelloTest(){
+		new Hello().say();
+	}
+    
+}
