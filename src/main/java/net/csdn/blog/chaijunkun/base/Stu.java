0e630b59c088132853b3c795129a1025a64bf401
chaijunkun <chaijunkun@autohome.com.cn>
2016-03-25 16:44:26 +0800

    增加属性

--- src/main/java/net/csdn/blog/chaijunkun/base/Stu.java
+++ src/main/java/net/csdn/blog/chaijunkun/base/Stu.java
@@ -5,6 +5,8 @@ public class Stu {
 	private String name;
 	
 	private Integer age;
+	
+	private Boolean sex;
 
 	/**
 	 * 获取{name}
@@ -36,6 +38,22 @@ public class Stu {
 	 */
 	public void setAge(Integer age) {
 		this.age = age;
+	}
+
+	/**
+	 * 获取{sex}
+	 * @return {sex}
+	 */
+	public Boolean getSex() {
+		return sex;
+	}
+
+	/**
+	 * 设置{sex}
+	 * @param sex {sex}
+	 */
+	public void setSex(Boolean sex) {
+		this.sex = sex;
 	}
 	
 }
