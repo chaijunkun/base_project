d4c25998ef1617138a55263819f5b83c6e748db2
chaijunkun <chaijunkun@autohome.com.cn>
2016-03-25 16:17:31 +0800

    增加类

---
+++ src/main/java/net/csdn/blog/chaijunkun/base/Stu.java
@@ -0,0 +1,41 @@
+package net.csdn.blog.chaijunkun.base;
+
+public class Stu {
+	
+	private String name;
+	
+	private Integer age;
+
+	/**
+	 * 获取{name}
+	 * @return {name}
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * 设置{name}
+	 * @param name {name}
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * 获取{age}
+	 * @return {age}
+	 */
+	public Integer getAge() {
+		return age;
+	}
+
+	/**
+	 * 设置{age}
+	 * @param age {age}
+	 */
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+}
