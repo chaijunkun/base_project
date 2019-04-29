c67e2b0e43a147d5dbbd7c76df7212c9692ccffc
柴俊堃 <chaijunkun@autohome.com.cn>
2019-04-29 13:52:47 +0800

    add test case

--- src/test/java/com/github/chaijunkun/distribution/test/IPUtilsTest.java
+++ src/test/java/com/github/chaijunkun/distribution/test/IPUtilsTest.java
@@ -2,8 +2,9 @@ package com.github.chaijunkun.distribution.test;
 
 import com.github.chaijunkun.distribution.utils.IPUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
+import org.testng.annotations.Test;
 
+import java.net.InetAddress;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -15,16 +16,15 @@ public class IPUtilsTest {
     @Test
     public void traverseIP() {
         try {
-
-            log.info("default: {}", IPUtils.getDefaultLocalIP());
-            Map<String, List<String>> localIP = IPUtils.getLocalIP();
+            log.info("default: {}", IPUtils.getDefaultLocalAddress());
+            Map<String, List<InetAddress>> localIP = IPUtils.getLocalAddress();
             if (!localIP.isEmpty()) {
-                Set<Map.Entry<String, List<String>>> entries = localIP.entrySet();
-                Iterator<Map.Entry<String, List<String>>> localIPIterator = entries.iterator();
+                Set<Map.Entry<String, List<InetAddress>>> entries = localIP.entrySet();
+                Iterator<Map.Entry<String, List<InetAddress>>> localIPIterator = entries.iterator();
                 while (localIPIterator.hasNext()) {
-                    Map.Entry<String, List<String>> next = localIPIterator.next();
+                    Map.Entry<String, List<InetAddress>> next = localIPIterator.next();
                     String interfaceName = next.getKey();
-                    List<String> ipList = next.getValue();
+                    List<InetAddress> ipList = next.getValue();
                     StringBuilder sb = new StringBuilder();
                     for (int i = 0; i < ipList.size(); i++) {
                         sb.append(ipList.get(i)).append(",");
